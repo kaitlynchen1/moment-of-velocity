046745d5adf547fe1711853724b074057f1b8b3d
kaitlynchen1 <kaitlynchen94@gmail.com>
2024-06-06 14:13:02 -0400

    updates

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
-with open('README.md') as f:
-    long_description = f.read()
+# with open('README.md') as f:
+#     long_description = f.read()
 
 setup(
-    author='Dustin Ingram',
-    author_email='di@di.codes',
-    description='A PyPI package with a Markdown README',
-    long_description=long_description,
-    long_description_content_type='text/markdown',  # This is important!
-    name='markdown-description-example',
-    url='',
-    version='0.0.1',
+    # author='Kaitlyn Chen',
+    # author_email='kaichen@g.hmc.edu',
+    # description='calculating moment of velocity',
+    # long_description=long_description,
+    # long_description_content_type='text/markdown', 
+    # name='moment-of-velocity',
+    # url='https://github.com/kaitlynchen1/moment-of-velocity',
+    version='0.1.2',
 )
